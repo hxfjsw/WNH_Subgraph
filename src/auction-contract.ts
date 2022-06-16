296341bd8831ffbba682bc54d1e39ed0706d0a4a
rodulph.huang <rudolph.huang@tron.network>
2022-06-16 11:37:00 +0800

    init

--- src/auction-contract.ts
+++ src/auction-contract.ts
@@ -1,72 +1,80 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import {BigInt} from "@graphprotocol/graph-ts"
 import {
-  AuctionContract,
-  AuctionCreated,
-  AuctionSuccessful,
-  AuctionCancelled,
-  Pause,
-  Unpause,
-  OwnershipTransferred
+    AuctionContract,
+    AuctionCreated,
+    AuctionSuccessful,
+    AuctionCancelled,
+    Pause,
+    Unpause,
+    OwnershipTransferred
 } from "../generated/AuctionContract/AuctionContract"
-import { AuctionCreatedEntity } from "../generated/schema"
+import {AuctionCreatedEntity} from "../generated/schema"
 
 export function handleAuctionCreated(event: AuctionCreated): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = AuctionCreatedEntity.load(event.transaction.from.toHex())
+    // Entities can be loaded from the store using a string ID; this ID
+    // needs to be unique across all entities of the same type
+    let entity = AuctionCreatedEntity.load(event.transaction.from.toHex())
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new AuctionCreatedEntity(event.transaction.from.toHex())
+    // Entities only exist after they have been saved to the store;
+    // `null` checks allow to create entities on demand
+    if (!entity) {
+        entity = new AuctionCreatedEntity(event.transaction.from.toHex())
 
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
+        // Entity fields can be set using simple assignments
+        entity.count = BigInt.fromI32(0)
+    }
 
-  // BigInt and BigDecimal math are supported
-  // @ts-ignore
-  entity.count = entity.count + BigInt.fromI32(1)
+    // BigInt and BigDecimal math are supported
+    // @ts-ignore
+    entity.count = entity.count + BigInt.fromI32(1)
 
-  // Entity fields can be set based on event parameters
-  entity.nftAddress = event.params._nftAddress
-  entity.tokenId = event.params._tokenId
-  entity.seller = event.params._seller
+    // Entity fields can be set based on event parameters
+    entity.nftAddress = event.params._nftAddress
+    entity.tokenId = event.params._tokenId
+    entity.seller = event.params._seller
+    entity.duration = event.params._duration
+    entity.endingPrice = event.params._startingPrice
+    entity.endingPrice = event.params._endingPrice
 
-  // Entities can be written to the store with `.save()`
-  entity.save()
+    // Entities can be written to the store with `.save()`
+    entity.save()
 
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
+    // Note: If a handler doesn't require existing field values, it is faster
+    // _not_ to load the entity from the store. Instead, create it fresh with
+    // `new Entity(...)`, set the fields that should be updated and save the
+    // entity back to the store. Fields that were not set or unset remain
+    // unchanged, allowing for partial updates to be applied.
 
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.auctions(...)
-  // - contract.paused(...)
-  // - contract.getCurrentPrice(...)
-  // - contract.setOwnerCut(...)
-  // - contract.getAuction(...)
-  // - contract.ownerCut(...)
-  // - contract.owner(...)
-  // - contract.getAPENFTAddress(...)
+    // It is also possible to access smart contracts from mappings. For
+    // example, the contract that has emitted the event can be connected to
+    // with:
+    //
+    // let contract = Contract.bind(event.address)
+    //
+    // The following functions can then be called on this contract to access
+    // state variables and other data:
+    //
+    // - contract.auctions(...)
+    // - contract.paused(...)
+    // - contract.getCurrentPrice(...)
+    // - contract.setOwnerCut(...)
+    // - contract.getAuction(...)
+    // - contract.ownerCut(...)
+    // - contract.owner(...)
+    // - contract.getAPENFTAddress(...)
 }
 
-export function handleAuctionSuccessful(event: AuctionSuccessful): void {}
+export function handleAuctionSuccessful(event: AuctionSuccessful): void {
+}
 
-export function handleAuctionCancelled(event: AuctionCancelled): void {}
+export function handleAuctionCancelled(event: AuctionCancelled): void {
+}
 
-export function handlePause(event: Pause): void {}
+export function handlePause(event: Pause): void {
+}
 
-export function handleUnpause(event: Unpause): void {}
+export function handleUnpause(event: Unpause): void {
+}
 
-export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
+export function handleOwnershipTransferred(event: OwnershipTransferred): void {
+}
